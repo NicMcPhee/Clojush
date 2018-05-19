b3856bf7b02ca0a74d62f0752defa8480b7fd753
Nic McPhee <mcphee@morris.umn.edu>
2018-05-19 11:17:21 -0500

    Clean up even parity implementation
    This does some (simple) refactoring to clean up the even parity implementation somewhat.
    It also converts from 4-bit to 5-bit.

--- src/clojush/problems/demos/even_parity.clj
+++ src/clojush/problems/demos/even_parity.clj
@@ -19,16 +19,25 @@
 ;; it was widely used as a synthetic test problem in the 90's and
 ;; 00's, especially in theoretical work and work on modularity.
 
-(def *num-even-parity-bits* 4)
+; I feel like this should be a command line argument rather than
+; a cooked in constant.
+(def *num-even-parity-bits* 5)
 
 (defn make-start-state
   [individual input]
+  ; Why are we looping to put these inputs on the
+  ; stack? We should have a function that does this
+  ; (maybe with concat?) and just call it.
   (loop [state (make-push-state)
          bools input]
     (if (empty? bools)
       state
       (recur (push-item (first bools) :input state)
              (rest bools)))))
+
+(defn even-parity? [bools]
+  "Returns true if the collection of booleans has an even number of true's"
+  (even? (count (filter identity bools))))
 
 (defn parity-compute-errors
   [individual]
@@ -37,10 +46,10 @@
     (doall
       (for [input inputs]
         (let [start-state (make-start-state individual input)
-              state (run-push (:program individual) start-state)
-              top-boolean (top-item :boolean state)]
+              end-state (run-push (:program individual) start-state)
+              top-boolean (top-item :boolean end-state)]
           (if (instance? Boolean top-boolean)
-            (if (= top-boolean (even? (count (filter identity input))))
+            (if (= top-boolean (even-parity? input))
               0
               1)
             1000))))))
@@ -49,12 +58,15 @@
   [individual]
   (assoc individual :errors (parity-compute-errors individual)))
 
+; There's got to be a better way than listing all the input variables
+; separately here.
 (def argmap
   {:error-function error-function
    :atom-generators (list 'in1
                           'in2
                           'in3
                           'in4
+                          'in5
                           'boolean_and
                           'boolean_or
                           'boolean_nand
